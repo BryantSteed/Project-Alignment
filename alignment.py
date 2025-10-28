7bee49a6c8173ddcd3e87425e8397c911f71b81c
Bryant <bryant.steed@gmail.com>
2025-10-27 21:59:54 -0600

    all baseline and core pass

--- alignment.py
+++ alignment.py
@@ -1,3 +1,5 @@
+import math
+
 def align(
         seq1: str,
         seq2: str,
@@ -21,17 +23,31 @@ def align(
         :param gap_open_penalty: how much it costs to open a gap. If 0, there is no gap_open penalty
         :param gap: the character to use to represent gaps in the alignment strings
     """
-    dist = {}
+    dist = {i: {} for i in range(-1, len(seq1))}
     prev = {}
-    for i in range(-1, len(seq1)):
-        dist[i] = {}
-        for j in range(-1, len(seq2)):
-            calculate_distance(seq1, seq2, indel_penalty, dist, prev, i, j, match_award, sub_penalty)
+    for i, j in position_iterator(seq1, seq2, banded_width):
+        calculate_distance(seq1, seq2, indel_penalty, dist, 
+                           prev, i, j, match_award, sub_penalty)
     edits = get_edit_sequence(seq1, seq2, prev)
     aligned_seq1, aligned_seq2 = get_sequence_alignment(seq1, seq2, gap, edits)
-    return dist[len(seq1)-1][len(seq2)-1], aligned_seq1, aligned_seq2
+    score = math.inf if aligned_seq1 is None else dist[len(seq1)-1][len(seq2)-1]
+    return score, aligned_seq1, aligned_seq2
+
+def position_iterator(seq1, seq2, banded_width):
+    if banded_width == -1:
+        for i in range(-1, len(seq1)):
+            for j in range(-1, len(seq2)):
+                yield i, j
+    else:
+        for i in range(-1, len(seq1)):
+            j_start = max(-1, i - banded_width)
+            j_end = min(len(seq2) - 1, i + banded_width)
+            for j in range(j_start, j_end + 1):
+                yield i, j
 
 def get_sequence_alignment(seq1, seq2, gap, edits):
+    if (not edits) and (len(seq1) != 0 and len(seq2) != 0):
+        return None, None
     aligned_seq1, aligned_seq2 = "", ""
     seq1_shifts, seq2_shifts = 0, 0
     for edit in edits:
@@ -67,7 +83,8 @@ def get_edit_sequence(seq1, seq2, prev):
     edits = rev_edits[::-1]
     return edits
 
-def calculate_distance(seq1, seq2, indel_penalty, dist, prev, i, j, match_award, sub_penalty):
+def calculate_distance(seq1, seq2, indel_penalty, dist, 
+                       prev, i, j, match_award, sub_penalty):
     if i == -1 and j == -1:
         dist[i][j] = 0
     elif i == -1:
@@ -77,12 +94,9 @@ def calculate_distance(seq1, seq2, indel_penalty, dist, prev, i, j, match_award,
         dist[i][j] = dist[i-1][j] + indel_penalty
         prev[i, j] = (i-1, j)
     else:
-        up_scenario = dist[i-1][j] + indel_penalty
-        left_scenario = dist[i][j-1] + indel_penalty
-        diag_modifier = match_award if seq1[i] == seq2[j] else sub_penalty
-        diag_scenario = dist[i-1][j-1] + diag_modifier
-        min_scenario = min(up_scenario, left_scenario, diag_scenario)
-        dist[i][j] = min_scenario
+        left_scenario, diag_scenario, min_scenario = \
+        get_scenarios(seq1, seq2, indel_penalty, dist, 
+                      i, j, match_award, sub_penalty)
         if min_scenario == diag_scenario:
             prev[i, j] = (i-1, j-1)
         elif min_scenario == left_scenario:
@@ -90,3 +104,17 @@ def calculate_distance(seq1, seq2, indel_penalty, dist, prev, i, j, match_award,
         else:
             prev[i, j] = (i-1, j)
 
+def get_scenarios(seq1, seq2, indel_penalty, dist, i, j, match_award, sub_penalty):
+    if j in dist[i-1]:
+        up_scenario = dist[i-1][j] + indel_penalty
+    else:
+        up_scenario = math.inf
+    if j-1 in dist[i]:
+        left_scenario = dist[i][j-1] + indel_penalty
+    else:
+        left_scenario = math.inf
+    diag_modifier = match_award if seq1[i] == seq2[j] else sub_penalty
+    diag_scenario = dist[i-1][j-1] + diag_modifier
+    min_scenario = min(up_scenario, left_scenario, diag_scenario)
+    dist[i][j] = min_scenario
+    return left_scenario, diag_scenario, min_scenario
